4ad74ee85364860d7561631793d3296eee1f17d2
Miguel Nieto A <anietom@uninorte.edu.co>
2021-10-31 18:29:27 -0500

    fix: reduce duplication with the let method

---
+++ spec/card_spec.rb
@@ -0,0 +1,24 @@
+class Card
+  attr_accessor :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
+
+RSpec.describe Card do
+  let(:card) { Card.new('Ace', 'Spades') }
+
+  it 'has a rank' do
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
+  end
+
+  it 'has a suit' do
+    expect(card.suit).to eq('Spades')
+    card.suit = ''
+    expect(card.suit).to eq('')
+  end
+end
