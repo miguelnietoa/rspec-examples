eeb99251fdd4fa20eb9d73ca9db04cf99d793658
Miguel Nieto A <anietom@uninorte.edu.co>
2021-11-01 08:42:49 -0500

    feat: new example with custom error message

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -18,7 +18,11 @@ RSpec.describe Card do
 
   it 'has a suit' do
     expect(card.suit).to eq('Spades')
-    card.suit = ''
-    expect(card.suit).to eq('')
+  end
+
+  it 'has a custom error message' do
+    card.suit = 'Nonsense'
+    comparison = 'Spades'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
   end
 end
