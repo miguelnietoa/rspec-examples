bf10e7a52300d2a722ebb167b6a93e8747af3b32
Miguel Nieto A <anietom@uninorte.edu.co>
2021-11-02 20:00:47 -0500

    Class Doubles

---
+++ spec/class_double_spec.rb
@@ -0,0 +1,30 @@
+class Deck
+  def self.build
+    # Business logic to build a whole bunch of cards
+  end
+end
+
+class CardGame
+  attr_reader :cards
+
+  def start
+    @cards = Deck.build
+  end
+end
+
+RSpec.describe CardGame do
+  it 'can only implement class methods that are defined on a class' do
+    # class_double(Deck, build: %w[Ace Queen])
+
+    # .as_stubbed_const is used to prevent the error:
+    # uninitialized constant CardGame::Deck
+    ## deck_klass = class_double('Deck', build: %w[Ace Queen]).as_stubbed_const
+
+    deck_klass = class_double(Deck, build: %w[Ace Queen]).as_stubbed_const
+
+    expect(deck_klass).to receive(:build)
+    subject.start
+    expect(subject.cards).to eq(%w[Ace Queen])
+
+  end
+end
